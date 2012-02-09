a6557be3489624296bb877e66396c5ec7ef0d173
Gaurav Chande <gmail@gauravchande.com>
2012-02-09 18:17:11 +0530

    User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Microblogging::Application.routes.draw do
-  get "users/new"
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   
+  resources :users
   match '/signup', to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,6 +8,6 @@ describe "User pages" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_selector('title', text: 'Sign up') }
   end
 end
